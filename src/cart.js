aacc79959d4d7b88841eb2cf15fe8283292eba5b
charu-dev <charu@gupta.com>
2022-12-17 16:54:04 +0530

    almost done hi hai

--- src/cart.js
+++ src/cart.js
@@ -3,6 +3,7 @@ let cart=JSON.parse(localStorage.getItem("data") ) || []
 
 
 let generateCartItems = () => {
+    cart=cart.filter((x)=>x.item>=0)
     if (cart.length !== 0) {
       return (cartPage.innerHTML = cart
         .map((x) => {
@@ -47,4 +48,41 @@ let generateCartItems = () => {
   
   generateCartItems();
   
-  function 
+  function countDec(id){
+id=id.id
+    let search=cart.find((x)=>x.itemid==id) 
+    if(search.item>0)search.item--;
+    if(search.item<0){
+        return
+    }
+    else{
+        let numbers=Array.from(document.querySelectorAll('.quantity'))
+    let mynumber=numbers.find((item)=>item.id==id)
+    mynumber.innerHTML=search.item
+    }
+
+    localStorage.setItem("data",JSON.stringify(cart))
+    
+    generateCartItems()
+    calculation()
+  }
+  function countInc(id){
+    id=id.id
+    let search=cart.find((x)=>x.itemid==id) 
+    search.item++;
+    let numbers=Array.from(document.querySelectorAll('.quantity'))
+    let mynumber=numbers.find((item)=>item.id==id)
+    mynumber.innerHTML=search.item
+
+    localStorage.setItem("data",JSON.stringify(cart))
+    generateCartItems()
+    calculation()
+    
+  }
+ 
+  let calculation = () => {
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = cart.map((x) => x.item).reduce((x, y) => x + y, 0);
+  };
+  
+  calculation();
